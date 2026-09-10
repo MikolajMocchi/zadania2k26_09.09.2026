4fb53917ec4c87882915d0529a28b6a48c713de8
Mocchi <mikolajgolonka43@gmail.com>
2026-09-10 09:31:20 +0200

    Update filtr.py

--- filtr.py
+++ filtr.py
@@ -1,1 +1,9 @@
-def jgdiff()
+def palidro(slowo): 
+    slowo = input("Podaj słowo: ").lower()
+    
+    if slowo == slowo[::-1]:
+        print(True)
+    else:
+        print(False)
+print(palidro("kajak"))
+palidro("kajak")
